ee66a7c63edba112db47f20223bd478b9ae3aadd
Alexandru Ciosa <alexandru.ciosa@p4b.ro>
2022-09-09 12:43:57 +0300

    update footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const footerStyle:any = {
+  marginTop: '1rem',
+  padding: '1rem',
+  backgroundColor: '#C3A51E',
+  bottom: 0,
+  left: 0,
+  textAlign: 'start',
+  color: 'white'
+};
+
+const Footer = () => (
+  <div style={footerStyle}>
+    <p>© 2022 Globus P4B All Rights Reserved</p>
+  </div>
+);
+
+export default Footer;
